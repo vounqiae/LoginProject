7ac30ac225dc3f38341f6fcd3c258488853616c9
Youngjae Park <vounqiae96@gmail.com>
2023-10-18 18:09:07 +0900

    1

--- LoginProject/ViewController.swift
+++ LoginProject/ViewController.swift
@@ -9,11 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let emailTextFieldview = UIView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        makeUI()
+
     }
 
-
+    func makeUI() {
+        emailTextFieldview.backgroundColor = UIColor.darkGray
+        
+        view.addSubview(emailTextFieldview)
+        
+        emailTextFieldview.translatesAutoresizingMaskIntoConstraints = false
+        
+        emailTextFieldview.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30).isActive = true
+        
+        emailTextFieldview.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30).isActive = true
+        
+        emailTextFieldview.topAnchor.constraint(equalTo: view.topAnchor, constant: 200).isActive = true
+        // 오토레이아웃 높이는 기준이 없다.
+        emailTextFieldview.heightAnchor.constraint(equalToConstant: 40).isActive = true
+    }
 }
 
